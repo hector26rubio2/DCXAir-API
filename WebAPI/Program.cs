d3456c3c2ce6199287ebf7064e9f952771aa8d81
HectorRubio2 <hector26rubio@gmail.com>
2024-04-13 19:03:05 -0500

    adicionar logger

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,28 +1,46 @@
-using Microsoft.Extensions.Configuration;
+using AutoMapper;
+using FluentValidation.AspNetCore;
+using Serilog;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using WebAPI._Configure;
-
+using WebAPI.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day) // Aquí configuramos la escritura en un archivo
+    .CreateLogger();
+builder.Host.UseSerilog(logger);
 
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers()
+    .AddFluentValidation(fv =>
+    {
+
+        fv.RegisterValidatorsFromAssemblyContaining(typeof(Program));
+    })
+  .AddJsonOptions(options =>
+  {
+      options.JsonSerializerOptions.PropertyNamingPolicy = new PascalCaseNamingPolicy();
+  });
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddConfiguration(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuración del pipeline de solicitudes HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
